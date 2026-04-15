8ce1984a83369b9ac937f2d5eb794cc095586dc2
MARIA CLARA DE OLIVEIRA MAIA <a95833493@rn.senac.local>
2026-04-15 20:47:54 -0300

    Criação do style dos card

--- js/ui/componentes.js
+++ js/ui/componentes.js
@@ -21,7 +21,6 @@ function verDetalhe(id) {
   window.location.href = `produto.html?id=${id}`;
 }
 
-
 // LOADING
 function renderLoading() {
   return `
@@ -39,7 +38,7 @@ function renderEmpty() {
 }
 
 
-// MENSAGEM RÁPIDA 
+// MENSAGEM RÁPIDA TOAST
 function mostrarToast(mensagem) {
   const toast = document.createElement("div");
   toast.className = "toast";
@@ -51,3 +50,33 @@ function mostrarToast(mensagem) {
     toast.remove();
   }, 3000);
 }
+
+// MODAL
+function mostrarModalConfirmação(mensagem, onConfirmar){
+    const overlay = document.createElement("div");
+    overlay.className = "modal-overlay";
+
+    const modal = document.createElement("div");
+  modal.className = "modal";
+
+  modal.innerHTML = `
+    <p>${mensagem}</p>
+    <div class="modal-botoes">
+      <button class="btn-confirmar">Confirmar</button>
+      <button class="btn-cancelar">Cancelar</button>
+    </div>
+  `;
+
+  overlay.appendChild(modal);
+  document.body.appendChild(overlay);
+
+  modal.querySelector(".btn-confirmar").onclick = () => {
+    onConfirmar();
+    overlay.remove();
+  };
+
+  modal.querySelector(".btn-cancelar").onclick = () => {
+    overlay.remove();
+  };
+}
+    
